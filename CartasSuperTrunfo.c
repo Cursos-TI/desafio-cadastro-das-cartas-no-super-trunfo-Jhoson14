a9579da9d0dec68aa6aa355bf7f70db74eef4130
Jhoson14 <marcos.ff7@hotmail.com>
2025-06-17 20:49:57 +0000

    começando aprendendo a codar 101

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+main(){
+    //parametros para cada cidade/estado
+    //incluido nestes parametros numero de: habitantes,pontos turisticos e o PIB
+    //numero da carta contendo inicial do estado e numero entre 1-8 referente ao total decartas disponivel no jogo
+    //Area da cidade em km²
+    char Código [3]; // da carta
+    char Estado[50];
+    char Cidade[50];
+    int Pop; //população
+    float Área;
+    float PIB;
+    //código da cidade, nome, população, área, PIB, número de pontos turísticos
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Desafio Super Trunfo - Estados \n");
+    printf("Vamos registrar as cartas para nosso jogo?\n");
+  
 
-    return 0;
+    //Carta 01
+    printf ("Dados da Carta 01 \n");
+    printf ("A qual o Estado pertence a carta? \n");
+    fgets(Estado, sizeof(Estado), stdin);
+
+    printf ("Qual nome da cidade? \n");
+    fgets(Cidade, sizeof(Cidade), stdin);
+
+    printf ("Há quantos Habitantes nesta cidade? \n");
+    scanf ("%d", &Pop);
+
+    printf ("Carta A01 \n");
+    printf("Estado:%s",Estado);
+    printf("Cidade:%s",Cidade);
+    printf("Habitantes: %d",Pop);
+    //retorno para testes, mover para o final após pronto
+
+    // fgets para nomes compostos
+
+    return 0 \n;
+
 }
