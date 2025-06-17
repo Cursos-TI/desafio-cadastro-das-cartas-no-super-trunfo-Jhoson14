36a6b926e23d7f22f9d224af77443d3901c2731e
Jhoson14 <marcos.ff7@hotmail.com>
2025-06-17 21:25:50 +0000

    preciso atualizar

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,15 +8,16 @@ main(){
     char Código [3]; // da carta
     char Estado[50];
     char Cidade[50];
-    int Pop; //população
+    int Pop; //População-Não deveria ser float ou algo similar? Resultado será limitado a numeros inteiros, numeros nas casas de milhares e milhões não serão apresentados corretamente
     float Área;
     float PIB;
+    int pontos turisticos
     //código da cidade, nome, população, área, PIB, número de pontos turísticos
     
     printf("Desafio Super Trunfo - Estados \n");
     printf("Vamos registrar as cartas para nosso jogo?\n");
   
-
+    // fgets para nomes compostos
     //Carta 01
     printf ("Dados da Carta 01 \n");
     printf ("A qual o Estado pertence a carta? \n");
@@ -28,13 +29,23 @@ main(){
     printf ("Há quantos Habitantes nesta cidade? \n");
     scanf ("%d", &Pop);
 
+    printf ("Quantos pontos turisticos há na cidade? \n);
+        scanf ("%d", &pontos
+
+//Retorno com dados das cartas
+
     printf ("Carta A01 \n");
     printf("Estado:%s",Estado);
     printf("Cidade:%s",Cidade);
-    printf("Habitantes: %d",Pop);
+    printf("Habitantes: %d\n",Pop); //0x para gerar casas decimais e hexadecimais?
+
+    printf ("Carta 02 \n");
+    printf("Estado:%s",Estado);
+    printf("Cidade:%s",Cidade);
+    printf("Habitantes:%d \n",Pop);
     //retorno para testes, mover para o final após pronto
 
-    // fgets para nomes compostos
+
 
     return 0;
 
